c02bc266bcf44f37555953920fb6186860f0c537
iamn00b <jurnaldide@gmail.com>
2015-03-31 14:22:27 +0700

    feedback front-end

--- app/routes.php
+++ app/routes.php
@@ -1,11 +1,14 @@
 <?php 
 
 
-$app->get('/', 					function () use ($app) 		{ (new Home())->welcome(); 					})->name('welcome');
-$app->get('/home/', 			function () use ($app) 		{ (new Home())->index(); 					})->name('home');
-$app->get('/dosen/', 			function () use ($app) 		{ (new Home())->tampildosen(); 				})->name('home');
-$app->get('/matakuliah/', 		function () use ($app) 		{ (new Home())->tampilmatkul(); 			})->name('home');
-$app->get('/logout/', 			function () use ($app) 		{ (new Home())->logout(); 					})->name('logout');
-$app->get('/rinciandosen/:id',	function ($id) use ($app) 	{ (new Home())->tampilrinciandosen($id); 	})->name('home');
-$app->get('/rincianmatkul/:id', function ($id) use ($app) 	{ (new Home())->tampilrincianmatkul($id); 	})->name('home');
-$app->get('/pencarian/:query', 	function ($query) use ($app){ (new Home())->tampilhasilpencarian($query); 		})->name('home');
+$app->get('/', 					function () use ($app) 		{ (new Home())->welcome(); 						})->name('welcome');
+$app->get('/home/', 			function () use ($app) 		{ (new Home())->index(); 						})->name('home');
+$app->get('/dosen/', 			function () use ($app) 		{ (new Home())->tampildosen(); 					})->name('dosen');
+$app->get('/matakuliah/', 		function () use ($app) 		{ (new Home())->tampilmatkul(); 				})->name('matakuliah');
+$app->get('/logout/', 			function () use ($app) 		{ (new Home())->logout(); 						})->name('logout');
+$app->get('/rinciandosen/:id',	function ($id) use ($app) 	{ (new Home())->tampilrinciandosen($id); 		})->name('rinciandosen');
+$app->get('/rincianmatkul/:id', function ($id) use ($app) 	{ (new Home())->tampilrincianmatkul($id); 		})->name('rincianmatkul');
+$app->get('/pencarian/:query', 	function ($query) use ($app){ (new Home())->tampilhasilpencarian($query); 	})->name('pencarian');
+
+$app->post('/feedback/', 		function () use ($app)		{ (new Home())->welcome(); 						})->name('feedback');
+
