3dbf28558e9b478d70350df2cb02f83448a11cfb
herwando <herwandor@gmail.com>
2015-03-31 00:19:22 -0700

    Last : 14:22

--- app/routes.php
+++ app/routes.php
@@ -2,9 +2,16 @@
 
 $app->get('/', function () use ($app) 					{ (new Home())->welcome(); 		})->name('welcome');
 $app->get('/home/', function () use ($app) 				{ (new Home())->index(); 		})->name('home');
-$app->get('/dosen/', function () use ($app) 			{ (new Home())->tampildosen(); 		})->name('home');
-$app->get('/matakuliah/', function () use ($app) 		{ (new Home())->tampilmatkul(); 		})->name('home');
+$app->get('/dosen/', function () use ($app) 			{ (new Home())->tampildosen(); 		})->name('dosen');
+$app->get('/matakuliah/', function () use ($app) 		{ (new Home())->tampilmatkul(); 		})->name('matkul');
 $app->get('/logout/', function () use ($app) 	{ (new Home())->logout(); 		})->name('logout');
-$app->get('/rincianmatkul/', function () use ($app) 		{ (new Home())->tampilrincianmatkul(); 		})->name('home');
-$app->get('/pencarian/:query', function ($query) use ($app) 		{ (new Home())->tampilhasilpencarian($query); 		})->name('home');
-$app->get('/rinciandosen/:id', function ($id) use ($app) 		{ (new Home())->tampilrinciandosen($id); 		})->name('home');
+$app->get('/rincianmatkul/', function () use ($app) 		{ (new Home())->tampilrincianmatkul(); 		})->name('rincianmatkul');
+$app->get('/pencarian/:query', function ($query) use ($app) 		{ (new Home())->tampilhasilpencarian($query); 		})->name('pencarian');
+$app->get('/rinciandosen/:id', function ($id) use ($app) 		{ (new Home())->tampilrinciandosen($id); 		})->name('rinciandosen');
+
+$app->get('/admin/cruddosen/', function () use ($app) 		{ (new Home())->tampilcruddosen(); 		})->name('cruddosen');
+$app->get('/admin/cruddosen/delete/:id', function ($id) use ($app) 		{ (new Home($id))->deletecruddosen($id); 		})->name('dosendelete');
+$app->get('/admin/rudreview/', function () use ($app) 		{ (new Home())->tampilrudreview(); 		})->name('rudreview');
+$app->get('/admin/rudreview/delete/:id', function ($id) use ($app) 		{ (new Home($id))->deleterudreview($id); 		})->name('reviewdelete');
+$app->get('/admin/rudkomentar/', function () use ($app) 		{ (new Home())->tampilrudkomentar(); 		})->name('rudkomentar');
+$app->get('/admin/rudkomentar/delete/:id', function ($id) use ($app) 		{ (new Home($id))->deleterudkomentar($id); 		})->name('komentardelete');
