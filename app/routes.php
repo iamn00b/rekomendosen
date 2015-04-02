1dcd8e2b06ce0a13c52e3e981a7474e20315787d
iamn00b <jurnaldide@gmail.com>
2015-04-03 06:09:10 +0700

    entah

--- app/routes.php
+++ app/routes.php
@@ -9,6 +9,7 @@ $app->get('/logout/', 			function () use ($app) 		{ (new Home())->logout(); 				
 $app->get('/rinciandosen/:id',	function ($id) use ($app) 	{ (new Home())->tampilrinciandosen($id); 		})->name('rinciandosen');
 $app->get('/rincianmatkul/:id', function ($id) use ($app) 	{ (new Home())->tampilrincianmatkul($id); 		})->name('rincianmatkul');
 $app->get('/pencarian/:query', 	function ($query) use ($app){ (new Home())->tampilhasilpencarian($query); 	})->name('pencarian');
+$app->get('/tambahreview/:nip', 	function ($nip) use ($app){ (new Home())->berireview($id); 	})->name('tambahreview');
 
 $app->post('/feedback/', 		function () use ($app)		{ (new Home())->isifeedback(); 						})->name('feedback');
 
